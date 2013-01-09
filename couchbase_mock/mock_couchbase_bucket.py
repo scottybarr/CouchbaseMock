63b3d867d81fa8102f87b46d8409655b18ea151f
Scott Barr <scottbarr@gmail.com>
2013-01-09 22:53:18 +0000

    Added expiry to cached items

--- couchbase_mock/mock_couchbase_bucket.py
+++ couchbase_mock/mock_couchbase_bucket.py
@@ -1,12 +1,26 @@
+import time
+
 class MockCouchbaseBucket:
     def __init__(self, name, server):
         self.cache = {}
+        self.expiration = {}
 
     def __getitem__(self, key):
-        return self.cache[key]
+        self._checkExpiry(key)
+        return self.cache[key] if key in self.cache else {}
 
     def get(self, key):
         return self.__getitem__(key)
 
     def set(self, key, expiration, flags, value):
+        self.expiration[key] = 0 if expiration == 0 else (self._getTimeStamp() + expiration)
         self.cache[key] = value
+
+    def _checkExpiry(self, key):
+        currentTime = self._getTimeStamp()
+        if self.expiration[key] and self.expiration[key] < currentTime and self.expiration[key] != 0:
+            del self.cache[key]
+            del self.expiration[key]
+
+    def _getTimeStamp(self):
+        return time.time()
--- tests/test_fake_couchbase.py
+++ tests/test_fake_couchbase.py
@@ -1,6 +1,7 @@
 import unittest
 from couchbase_mock.mock_couchbase import MockCouchbase
 from couchbase_mock.mock_couchbase_bucket import MockCouchbaseBucket
+import time
 
 
 class TestFakeCouchbase(unittest.TestCase):
@@ -11,10 +12,25 @@ class TestFakeCouchbase(unittest.TestCase):
         
     def test_can_get_and_set(self):
         val = "hello world!"
-
         fake = MockCouchbase()["bucket_name"]
         fake.set("abc", 5, 0, val)
         self.assertEqual(fake.get("abc"), val)
+
+    def test_item_has_expired(self):
+        val = "test"
+        fake = MockCouchbase()["bucket_name"]
+        fake.set("tester", 1, 0, val)
+        fake._getTimeStamp = self.expiredTime
+        self.assertDictEqual(fake.get("tester"), {})
+
+    def test_persistant_get_item(self):
+        val = "hello world!"
+        fake = MockCouchbase()["bucket_name"]
+        fake.set("abc", 0, 0, val)
+        self.assertEqual(fake.get("abc"), val)
+
+    def expiredTime(self):
+        return time.time() * 2
 
 
 if __name__ == "__main__":
